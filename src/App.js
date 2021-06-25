001c270368d71cf7ec832747022aa8ed46b0040d
dikshitakambri <dikshitakambri18@gmail.com>
2021-06-25 17:05:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Table from './components/Table';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Table />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
